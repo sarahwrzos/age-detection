f147556a82c5a04cc1208f152d9467f49628b1b4
redson158 <redson@ithaca.edu>
2025-12-09 14:43:03 -0500

    file cache updates

--- python files/main.py
+++ python files/main.py
@@ -6,28 +6,29 @@ from train_loops import *
 from print_metrics import *
 
 MODEL_DIR = Path(__file__).resolve().parent.parent / "trained_models"
-filename = "resnet50_50e.pth"
+#filename = "resnet50_50e.pth"
 #filename = "vit.pth"
+filename = "vit_7e.pth"
 model_pth = MODEL_DIR / filename
-model_name = "resnet50"
-#model_name = 'vit_tiny_patch16_224'
+#model_name = "resnet50"
+model_name = 'vit_tiny_patch16_224'
 
 def main():
     df, train_images_path = load_data()
     #print_images(df, train_images_path)
     map_labels(df)
-    transform = resizeCNN()
-    #transform = resizeVit()
+    #transform = resizeCNN()
+    transform = resizeVit()
     device, train_loader, val_loader, train_df_split, val_df_split = split_data(transform, train_images_path, df)
     if not model_pth.exists():
-        model, optimizer, criterion = load_pretrained_cnn_model(device, model_name)
-        #model, optimizer, criterion = load_pretrained_vit_model(device)
-        trained_model = train_loop(model, optimizer, criterion, train_loader, val_loader, device, 50)
+        #model, optimizer, criterion = load_pretrained_cnn_model(device, model_name)
+        model, optimizer, criterion = load_pretrained_vit_model(device)
+        trained_model = train_loop(model, optimizer, criterion, train_loader, val_loader, device, 7)
         save_model(trained_model, model_pth)
     else:
         #umcomment one or the other for resnet/vit
-        trained_model = load_trained_resnet_model(3, model_pth, model_name)
-        #trained_model = load_trained_vit_model(3, model_pth, model_name)
+        #trained_model = load_trained_resnet_model(3, model_pth, model_name)
+        trained_model = load_trained_vit_model(3, model_pth, model_name)
         print(f"{model_name} Model loaded")
 
         #validation and training accuracies
